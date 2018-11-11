6327603d5fb6edc1ffd43d0197c58bddf92eb041
Le Minh Tri <trileminh94@gmail.com>
2018-11-11 13:02:41 +0700

    try some format

--- interface/interface.go
+++ interface/interface.go
@@ -1,9 +1,13 @@
 package main
 
-import fmt "fmt"
+import (
+	fmt "fmt"
+	"os"
+)
 
 type shape interface {
 	area() float32
+	print()
 }
 
 type ractangle struct {
@@ -18,15 +22,43 @@ func (c circle) area() float32 {
 	return 3.14 * c.radius * c.radius
 }
 
+func (c circle) print() {
+	fmt.Printf("%#v\n", c)
+}
+
 func (r ractangle) area() float32 {
 	return r.height * r.width
+}
+
+func (r ractangle) print() {
+	fmt.Printf("%#v\n", r)
 }
 
 func calArea(s shape) float32 {
 	return s.area()
 }
 
+func print(s shape) {
+	s.print()
+}
+
 func main() {
-	fmt.Println(calArea(circle{10}))
-	fmt.Println(calArea(ractangle{10, 20}))
+	c := circle{10}
+	r := ractangle{10, 20}
+	fmt.Println(calArea(c))
+	fmt.Println(calArea(r))
+	print(c)
+	print(r)
+
+	fmt.Printf("%q\n", "\"string\"")
+	fmt.Printf("%s\n", "\"string\"")
+	fmt.Printf("%p\n", &c)
+
+	fmt.Printf("|%6d|%6d|\n", 12, 345)
+	fmt.Printf("%15s\n", "---------------")
+	fmt.Printf("|%6.2f|%6.2f|\n", 12.0, 345.3)
+	fmt.Printf("%15s\n", "---------------")
+	fmt.Printf("|%-6.2f|%6.2f|\n", 12.0, 345.3)
+
+	fmt.Fprintf(os.Stderr, "a %s\n", "error")
 }
